5704bd792965545bb1eea7b75fcb32974041ecd2
SPECL EXP ACCT <exp.pcl.mu@gmail.com>
2024-04-26 23:00:02 -0700

    automatic update

--- evidence/dev/dev.py
+++ evidence/dev/dev.py
@@ -4,24 +4,51 @@ from numpy import random
 rng = random.default_rng()
 
 win = visual.Window(units="pix", size=(500, 500), color=[-1, -1, -1], fullscr=True)
+correctSound1 = sound.Sound(500,secs = 0.25)
+correctSound2 = sound.Sound(1000,secs = 0.25)
 
-val = rng.integers(0,2)
-coordinates = []
-for i in range(31):
-    xVal = np.round(np.random.normal(25*(val*2-1),125))
-    circ=visual.Circle(win, pos=(xVal,0), fillColor=[1, 1, 1], radius=5)
-    circ.draw()
-    win.flip()
-    core.wait(.3)
-    coordinates.append(xVal)
-    if(event.getKeys(['x'])):
-        print("Response: X")
-        break
-    if(event.getKeys(['m'])):
-        print("Response: M")
-        break
+def playSound():
+    correctSound1.play()
+    core.wait(0.25)
+    correctSound2.play()
+    core.wait(0.25)
 
-print("Number of shots: ",i)
-print(coordinates)
+def displayDots(mu,sd):
+    neg = rng.integers(0,2)
+    if(neg == 0):
+        correct = 'x'
+    else:
+        correct = 'm'
+    coordinates = []
+    for i in range(31):
+        coordinates.append(np.round(np.random.normal(mu*(neg*2-1),sd)))
+    for i in range(coordinates):
+        circ=visual.Circle(win, pos=(coordinates[i],0), fillColor=[1, 1, 1], radius=5)
+        circ.draw()
+        win.flip()
+        core.wait(.3)
+        if(event.getKeys(['x'])):
+            if(correct == 'x'):
+                playSound()
+            return correct,'x',i,coordinates
+        if(event.getKeys(['m'])):
+            if(correct == 'm'):
+                playSound()
+            return correct,'m',i,coordinates
+    return correct,'no response',i,coordinates
+
+def trial(numTrials,mu,sd):
+    correctResponse = ()
+    numDots = ()
+    coordinates = np.empty()
+    for i in range(numTrials+1):
+        data = displayDots(mu,sd)
+        correctResponse.append(data[0]==data[1])
+        numDots.append(data[2])
+        np.append(coordinates,np.array(coordinates[3]))
+    return correctResponse,numDots,coordinates
+
+print(trial(5,25,125))
+
 win.close()
 core.quit()
