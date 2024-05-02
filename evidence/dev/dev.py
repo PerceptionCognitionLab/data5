f0d0a4b4cc6f3406136c189b02aecc6a74ac1de2
SPECL EXP ACCT <exp.pcl.mu@gmail.com>
2024-05-01 23:00:01 -0700

    automatic update

--- evidence/dev/dev.py
+++ evidence/dev/dev.py
@@ -4,9 +4,14 @@ from numpy import random
 import sys
 sys.path.insert(0, '/home/exp/specl-exp/lib/data5/')
 import expLib51 as elib
-seed = 1
+
+#random
+rng = random.default_rng()
+seed = random.randint(1,1000000)
 rng = random.default_rng(seed)
 
+#globals
+abortKey = ['9']
 refreshRate=165
 elib.setRefreshRate(refreshRate)
 expName="devEv"
@@ -14,6 +19,11 @@ dbConf=elib.beta
 #[pid,sid,fname]=elib.startExp(expName,dbConf,pool=1,lockBox=True,refreshRate=refreshRate)
 [pid,sid,fname]=[1,1,'test']
 fptr=open(fname,"w")
+mu = 25
+sd = 125
+numDots = 30
+dotY = 0
+dotRadius = 5
 
 win = visual.Window(units="pix", size=(500, 500), color=[-1, -1, -1], fullscr=True)
 correctSound1 = sound.Sound(500,secs = 0.25)
@@ -25,44 +35,50 @@ def playSound():
     correctSound2.play()
     core.wait(0.25)
 
-def displayDots(mu,sd):
+def displayDots(mu,sd,numDots,dotY,dotRadius):
+    win.flip()
+    core.wait(0.5)
+    marker = visual.Circle(win,pos=(0,0),fillColor=[255,0,0],radius=5)
+    marker.draw()
+    win.flip()
+    core.wait(0.5)
+    marker = visual.Circle(win,pos=(0,0),fillColor=[255,255,0],radius=5)
+    marker.draw()
+    win.flip()
+    core.wait(0.5)
+    marker = visual.Circle(win,pos=(0,0),fillColor=[0,255,0],radius=5)
+    marker.draw()
+    win.flip()
+    core.wait(0.5)
     neg = rng.integers(0,2)
     if(neg == 0):
         correct = -1
     else:
         correct = 1
     coordinates = []
-    for i in range(31):
+    for i in range(numDots+1):
         coordinates.append(np.round(np.random.normal(mu*(neg*2-1),sd)))
     for i in range(len(coordinates)):
-        circ=visual.Circle(win, pos=(coordinates[i],0), fillColor=[1, 1, 1], radius=5)
+        circ=visual.Circle(win, pos=(coordinates[i],dotY), fillColor=[1, 1, 1], radius=dotRadius)
         circ.draw()
         win.flip()
         core.wait(.3)
+        if(event.getKeys(abortKey)):
+            win.close()
+            core.quit()
         if(event.getKeys(['x'])):
             if(correct == -1):
                 playSound()
-            return [coordinates,correct,-1,i]
+            return [*coordinates,correct,-1,i]
         if(event.getKeys(['m'])):
             if(correct == 1):
                 playSound()
-            return [coordinates,correct,1,i]
-    return [coordinates,correct,0,i]
-
-def block(numTrials,mu,sd):
-    correctResponse = []
-    numDots = []
-    coordinates = []
-    for i in range(numTrials):
-        data = displayDots(mu,sd)
-        correctResponse.append(data[0]==data[1])
-        numDots.append(data[2])
-        coordinates.append(data[3])
-    return [correctResponse,numDots,coordinates]
+            return [*coordinates,correct,1,i]
+    return [*coordinates,correct,0,i]
 
 numTrials = 5
-for i in range(5):
-    output=[pid,sid,i+1]+displayDots(25,125)
+for i in range(numTrials):
+    output=[pid,sid,i+1]+displayDots(mu,sd,numDots,dotY,dotRadius)
     print(*output,sep=", ", file=fptr)
 
 
