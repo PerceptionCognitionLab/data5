de076d65a9b208f5b227dac38a17ebd913a23710
SPECL EXP ACCT <exp.pcl.mu@gmail.com>
2024-05-22 11:04:36 -0700

    automatic update

--- eyespeed/eyespd.py
+++ eyespeed/eyespd.py
@@ -2,15 +2,19 @@ import numpy as np
 from numpy import random
 from psychopy import core, visual, event
 from PIL import Image
+import sys
+import os
+import time
+
 
 # Initialize random number generator
-rng = random.default_rng(seed=np.random.randint(1, 1000, 1))
+rng = np.random.default_rng(seed=np.random.randint(1, 1000))
 
-# Create a window
+# Window
 win = visual.Window(units="pix", size=(750, 750), color=[1, 1, 1], fullscr=True)
 
 # Function to draw a random monochrome letter
-def rand_ltr():
+def rand_ltr(win, duration=2):
     mono_W = np.flip(np.array(Image.open('testW.png').convert("L"))) / 255.0
     mono_A = np.flip(np.array(Image.open('testA.png').convert("L"))) / 255.0
     mono_M = np.flip(np.array(Image.open('testM.png').convert("L"))) / 255.0
@@ -20,34 +24,39 @@ def rand_ltr():
     chosen_letter, chosen_ltr_image = letters[select]
     stim = visual.ImageStim(win, image=chosen_ltr_image)
     stim.draw()
-    return stim, chosen_letter
-
-# # Function to get the response
-# def get_response(chosen_letter):
-#     continueRoutine = True
-#     response_list = []
-#     response = 0
-#     while continueRoutine:
-#         keys = event.waitKeys()
-#         response_list.extend(keys)
-#         if chosen_letter in response_list:
-#             response = 1  # True
-#             continueRoutine = False
-#         elif any(key in ['escape', 'q'] for key in response_list):
-#             response = 0  # False
-#             continueRoutine = False
-#     return response
-
-# Main test function
-def test_image(duration=3):
-    stim, chosen_letter = rand_ltr()
     win.flip()
-    # response_accuracy = get_response(chosen_letter)
     core.wait(duration)
-    win.close()
-    # print(f"Chosen letter: {chosen_letter}, Response accuracy: {response_accuracy}")
+    return chosen_letter
+
+# Function to get the response
+def getresponse(win, chosen_letter):
+    prompt = visual.TextStim(win, text="What letter did you see?", color=[-1, -1, -1], height = 40)
+    prompt.draw()
+    win.flip()
+    keys = event.waitKeys()
+    response = keys[0] if keys else None
+    responseupper = response.upper()
+    
+    correct = responseupper == chosen_letter
+
+    if correct:
+        feedback_text = "That's correct"
+    else:
+        feedback_text = "That's incorrect"
+
+    feedback = visual.TextStim(win, text=feedback_text, color=[-1, -1, -1], height = 40)
+    feedback.draw()
+    win.flip()
+    core.wait(2)
+
+    return correct
 
 # Run the test
-test_image()
+chosen_letter = rand_ltr(win, duration=2)
+correct = getresponse(win, chosen_letter)
+print('Randomly chosen letter:', chosen_letter)
+print('Correct response:', correct)
+win.close()
 core.quit()
+
 
