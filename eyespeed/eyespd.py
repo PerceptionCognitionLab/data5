d0ac57f5f8d2b903ef51a35beac4145c68b171cd
SPECL EXP ACCT <exp.pcl.mu@gmail.com>
2024-05-22 23:00:01 -0700

    automatic update

--- eyespeed/eyespd.py
+++ eyespeed/eyespd.py
@@ -8,29 +8,41 @@ import time
 
 
 # Initialize random number generator
+
 rng = np.random.default_rng(seed=np.random.randint(1, 1000))
 
 # Window
-win = visual.Window(units="pix", size=(750, 750), color=[1, 1, 1], fullscr=True)
+
+win = visual.Window(units="pix", size=(750, 750), color=[-1, -1, -1], fullscr=True)
+
+# Noise Function
+
+def add_noise(image,mean = 0,stddev = 2): ### need to make a function that adjusts the standard devision based on correctness
+    noise = random.normal(mean,stddev,image.shape)
+    noisy_image = np.clip(image + noise,0,1)
+    return(noisy_image)
 
 # Function to draw a random monochrome letter
-def rand_ltr(win, duration=2):
-    mono_W = np.flip(np.array(Image.open('testW.png').convert("L"))) / 255.0
+
+def rand_ltr(win, duration=2, stddev=2):
+    mono_W = np.flip(np.array(Image.open('testW.png').convert("L"))) / 255.0 ### add a for loop and collect an array of images generated
     mono_A = np.flip(np.array(Image.open('testA.png').convert("L"))) / 255.0
     mono_M = np.flip(np.array(Image.open('testM.png').convert("L"))) / 255.0
     mono_O = np.flip(np.array(Image.open('testO.png').convert("L"))) / 255.0
     letters = [("A", mono_A), ("W", mono_W), ("M", mono_M), ("O", mono_O)]
     select = rng.integers(0, 4)
     chosen_letter, chosen_ltr_image = letters[select]
-    stim = visual.ImageStim(win, image=chosen_ltr_image)
+    noisy_image = add_noise(chosen_ltr_image, mean = 0, stddev=stddev)
+    stim = visual.ImageStim(win, image=noisy_image)
     stim.draw()
     win.flip()
     core.wait(duration)
     return chosen_letter
 
 # Function to get the response
+
 def getresponse(win, chosen_letter):
-    prompt = visual.TextStim(win, text="What letter did you see?", color=[-1, -1, -1], height = 40)
+    prompt = visual.TextStim(win, text="What letter did you see?", color=[1, 1, 1], height = 40)
     prompt.draw()
     win.flip()
     keys = event.waitKeys()
@@ -44,19 +56,46 @@ def getresponse(win, chosen_letter):
     else:
         feedback_text = "That's incorrect"
 
-    feedback = visual.TextStim(win, text=feedback_text, color=[-1, -1, -1], height = 40)
+    feedback = visual.TextStim(win, text=feedback_text, color=[1, 1, 1], height = 40)
     feedback.draw()
     win.flip()
     core.wait(2)
-
     return correct
 
 # Run the test
-chosen_letter = rand_ltr(win, duration=2)
-correct = getresponse(win, chosen_letter)
-print('Randomly chosen letter:', chosen_letter)
-print('Correct response:', correct)
+
+def doTrial(numTrials):
+    chosen_letter_array = []
+    correct_array = []
+    correct = False
+    stddev = 6
+    c = 0
+    for i in range(numTrials):
+        if correct:
+            c = c + 1
+        else:
+            stddev = stddev - 1
+            if stddev < 1:
+                stddev = 1
+        if c == 2:
+            stddev = stddev + 1
+            c = 0
+
+        chosen_letter = rand_ltr(win, duration=2, stddev = stddev)
+        print(stddev)
+        correct = getresponse(win, chosen_letter)
+        chosen_letter_array.append(chosen_letter)
+        correct_array.append(correct)
+        print('Randomly chosen letter:', chosen_letter)
+        print('Correct response:', correct)
+    return chosen_letter_array, correct_array
+
+
+numTrials = 5
+
+chosen_letter_array, correct_array = doTrial(numTrials)
+print('Chosen letter array:', chosen_letter_array)
+print('Response correctness:', correct_array)
 win.close()
 core.quit()
-
 
