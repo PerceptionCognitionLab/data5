14f40eddcba9dd04a5c391138ef3b4777507c134
SPECL EXP ACCT <exp.pcl.mu@gmail.com>
2024-07-03 23:00:02 -0700

    automatic update

--- eyespeed/eyespd.py
+++ eyespeed/eyespd.py
@@ -14,7 +14,7 @@ incorrectSound1 = sound.Sound(500, secs=0.5)
 incorrectSound2 = sound.Sound(375, secs=0.5)
 rng = np.random.default_rng(seed=np.random.randint(1, 1000))
 win = visual.Window(units="pix", size=(1920, 1080), color=[-1, -1, -1], fullscr=True)
-clock = core.Clock()
+globalClock = core.Clock()
 
 # Functions
 def playCorrectSound():
@@ -28,7 +28,7 @@ def playIncorrectSound():
     core.wait(0.5)
     incorrectSound2.play()
 
-def rand_ltr(win, duration=2, noise_std=1):
+def rand_ltr(win, duration=2, sd=1):
     m = np.array(Image.open('testM.png').convert('L')) / 255
     w = np.array(Image.open('testW.png').convert('L')) / 255
     a = np.array(Image.open('testA.png').convert('L')) / 255
@@ -39,15 +39,14 @@ def rand_ltr(win, duration=2, noise_std=1):
     chosen_letter = rng.choice(list(letters.keys()))
     chosen_image = letters[chosen_letter]
     other_image = rng.choice(list(letters.values()))
-    image2 = rng.choice(list(letters.keys()))
 
     d = chosen_image.shape
-    weight = round(rng.uniform(.25,.75), 4)
+    weight = round(rng.uniform(.25, .75), 4)
     g1 = fftn(chosen_image)
     g2 = fftn(other_image)
 
     power = (weight * np.abs(g1) + (1 - weight) * np.abs(g2))
-    noise = np.random.normal(loc=0, scale=noise_std, size=d)
+    noise = np.random.normal(loc=0, scale=sd, size=d)
     gNoise = fftn(noise)
     fNoise = np.real(ifftn(gNoise * power))
     stimuli = fNoise + 50 * chosen_image
@@ -55,26 +54,30 @@ def rand_ltr(win, duration=2, noise_std=1):
     temp_image_path = "temp_stimulus.png"
     plt.imsave(temp_image_path, stimuli, cmap='gray')
 
-    fix = visual.TextStim(win, "+") 
+    fix = visual.TextStim(win, "+")
     fix.draw()
     win.flip()
-    core.wait(.5)
+    core.wait(0.5)
     stim = visual.ImageStim(win, image=temp_image_path, size=(800, 600))
     stim.draw()
     win.flip()
-    core.wait(duration)
+    clock = core.Clock()
+    response_time = None
+    keys = event.waitKeys(maxWait=duration, keyList=['a', 'w', 'm', 'o'])
+    
+    if keys:
+        response_time = clock.getTime()
+        response = keys[0]
+    else:
+        response_time = 0
+        response = None
 
+    win.flip()
     os.remove(temp_image_path)
 
-    return chosen_letter, image2, weight
+    return chosen_letter, response, response_time, weight
 
-def getresponse(win, chosen_letter):
-    prompt = visual.TextStim(win, text="What image did you see?", color=[1, 1, 1], height=40)
-    prompt.draw()
-    win.flip()
-    keys = event.waitKeys(keyList=['a', 'w', 'm', 'o'])
-    response = keys[0] if keys else None
-    
+def getresponse(response, chosen_letter):
     correct = response == chosen_letter
 
     if correct:
@@ -90,59 +93,64 @@ def getresponse(win, chosen_letter):
     core.wait(2)
     return correct
 
-def doTrial(numTrials, block, noise_std):
+def doTrial(numTrials, block, sd):
+    chosen_letter_array = []
+    correct_array = []
+    std_array = []
+    duration_array = []
+    response_times = []
+    weights = []
+    sd = 1.5
+    duration = 2
+    consecutive_correct = 0
+    deltaSd = 0.20
+
     for blk in range(block):
         prompt = visual.TextStim(win, text="Press any key to continue", color=[1, 1, 1], height=40)
         prompt.draw()
         win.flip()
         event.waitKeys()
-        
-        chosen_letter_array = []
-        image2_array = []
-        correct_array = []
-        std_array = []
-        weights = []
-        noise_std = 1.5
-        consecutive_correct = 0
-        deltaStd = 0.20
 
         for trl in range(numTrials):
-            chosen_letter, image2, weight = rand_ltr(win, duration=2, noise_std=noise_std)
-            correct = getresponse(win, chosen_letter)
+            chosen_letter, response, response_time, weight = rand_ltr(win, duration, sd=sd)
+            correct = getresponse(response, chosen_letter)
             chosen_letter_array.append(chosen_letter)
             correct_array.append(correct)
-            std_array.append(noise_std)
-            image2_array.append(image2)
+            std_array.append(sd)
+            response_times.append(response_time)
             weights.append(weight)
+            duration_array.append(duration)
            
-         
             if correct:
-                consecutive_correct += 1
+                consecutive_correct = consecutive_correct + 1
                 if consecutive_correct == 2:
-                    noise_std += deltaStd
+                    sd = sd + deltaSd
+                    duration = duration + 1
                     consecutive_correct = 0
             else:
                 consecutive_correct = 0
-                noise_std -= deltaStd
+                sd = sd - deltaSd
+                duration = duration - 1
 
-            if noise_std <= 1:
-                noise_std = 1
+            if sd <= 1:
+                sd = 1
+            if duration <= 2:
+                duration = 2
 
+    return chosen_letter_array, correct_array, std_array, response_times, weights, duration_array
 
-    return chosen_letter_array, correct_array, std_array, image2_array, weights
-
-
-numTrials = 3
+numTrials = 10
 block = 1
 
-chosen_letter_array, correct_array, std_array, image2_array, weights = doTrial(numTrials, block, noise_std=1)
+chosen_letter_array, correct_array, std_array, response_times, weights, duration_array = doTrial(numTrials, block, sd=1)
 print("##################################")
-print("First Image:", chosen_letter_array)
-print("Second Image:", image2_array)
+print("##################################")
+print("Response Times:", response_times)
+print("Chosen Letters:", chosen_letter_array)
 print("Correct responses:", correct_array)
 print("Standard deviation by trial:", std_array)
 print("Weights by trial:", weights)
+print("Duration:", duration_array)
+print("##################################")
 print("##################################")
 core.quit()
-
-a = event.waitKeys(keyList=abortKey)
