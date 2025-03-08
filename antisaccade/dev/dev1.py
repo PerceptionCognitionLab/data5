dcbe2d6ed24a2070db6aacda3137fc459b516e6c
SPECL EXP ACCT <exp.pcl.mu@gmail.com>
2025-03-07 23:00:01 -0800

    automatic update

--- antisaccade/dev/dev1.py
+++ antisaccade/dev/dev1.py
@@ -1,10 +1,11 @@
-from psychopy import prefs
+from psychopy import prefs  
 prefs.hardware['audioLib']=['PTB']
 prefs.hardware['audioLatencyMode']=3
 from psychopy import core, visual, sound, event
 import numpy as np
 from numpy import random
 import sys
+import math
 sys.path.insert(0, '/home/exp/specl-exp/lib/data5/')
 import expLib51 as el
 
@@ -18,24 +19,46 @@ win=visual.Window(units= "pix",
                      size=(2*scale,2*scale),
                      color=[-1,-1,-1],
                      fullscr = True)
-                     
-                     
 
 
 frames=[]
-frameDurations=[30,30,30]
+frameDurations=[82,82,49,2,30,6,6]
 
 rng = random.default_rng()
+
+#stimulus
 stim1 = 0
-n = rng.integers(0,2,1)
-if n == 0:
+stim = rng.integers(0,2,1)
+if stim == 0:
     stim1 = 'M'
 else:
     stim1 = 'X'
 
-frames.append(visual.TextStim(win,"+"))
-frames.append(visual.TextStim(win,stim1))
-frames.append(visual.TextStim(win,"#"))
+#stim angle
+degAngle = rng.integers(0,360,1)
+radAngle = math.radians(degAngle)
+xpos = 300*math.cos(radAngle)
+ypos = 300*math.sin(radAngle)
+
+
+#condition
+cond=0
+pickCond=rng.integers(0,2,1)
+if pickCond == 0: #congruent condition
+    xcue = 300*math.cos(radAngle)
+    ycue = 300*math.sin(radAngle)
+else: #incongruent condition 
+    xcue = -300*math.cos(radAngle)
+    ycue = -300*math.sin(radAngle) 
+
+
+frames.append(visual.TextStim(win,"+", height = 30))
+frames.append(visual.TextStim(win, " "))
+frames.append(visual.TextStim(win, "-", pos=(xcue,ycue), height=50))
+frames.append(visual.TextStim(win, " "))
+frames.append(visual.TextStim(win,stim1, pos=(xpos, ypos), height=30))
+frames.append(visual.TextStim(win,"#", pos=(xpos,ypos), height=30))
+frames.append(visual.TextStim(win, "@", pos=(xpos,ypos), height=30)) 
 
 message=visual.TextStim(win,"Press a key to start")
 message.draw()
@@ -50,3 +73,4 @@ win.close()
 print("Difference Between Time Stamps:\n",np.diff(stamps),"\n")
 print("Difference Between Frames:\n",el.actualFrameDurations(frameDurations,stamps))
 core.quit()
+
