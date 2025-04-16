64c465cabcd3ed9c3f62f93721ec9128ec161338
SPECL EXP ACCT <exp.pcl.mu@gmail.com>
2025-04-15 23:00:02 -0700

    automatic update

--- antisaccade/dev/dev1.py
+++ antisaccade/dev/dev1.py
@@ -10,6 +10,16 @@ sys.path.insert(0, '/home/exp/specl-exp/lib/data5/')
 import expLib51 as el
 from types import SimpleNamespace
 
+pid=1
+sid=1
+fname="test"
+fptr=open(fname,"w")
+blk=0
+trl=0
+
+cond=0
+theta=0
+target=0
 
 scale=400
 
@@ -27,37 +37,42 @@ gParDict={"radius":150,
       "mask":['@','#']}
 gPar = SimpleNamespace(**gParDict)
 
+lParDict={"cond":cond,
+          "angle":theta,
+          "target":target,
+          "dur":60}
+lPar = SimpleNamespace(**lParDict)
 
-frames=[]
-# frameDurations=[82,82,49,2,30,6,6]
-frameDurations=[100,100,100,0,100,100,100]
 
+frameDurations=[82,82,49,lPar.dur,30,6,6]
 rng = random.default_rng()
-theta=0
-cond=0 
-target=0
 
 def d2r(theta):
     return(math.radians(theta))
 
-lParDict={"cond":cond,
-          "angle":theta,
-          "target":target,
-          "dur":200}
-lPar = SimpleNamespace(**lParDict)
+def getResp(abortKey='9'):
+    keys=event.getKeys(keyList=['x','m',abortKey],timeStamped=trialClock)
+    if len(keys)==0:
+        keys=event.waitKeys(keyList=('x','m',abortKey),timeStamped=trialClock)
+    resp=keys[0][0]
+    rt=keys[0][1]
+    if resp==abortKey:
+        fptr.close()
+        win.close()
+        core.quit()   
+    resp = int(resp=='m')
+    return([resp,rt])
 
+def runTrial(lPar,trl):
 
-
-def runTrial(lPar):
-
+    frames=[]
     if lPar.cond==1:
         cueAngle = (lPar.angle+180)%360
     else:
         cueAngle =  lPar.angle
     
-    frameDurations[3]=lPar.dur
     frames.append(visual.TextStim(win,"+", height = 30))
-    frames.append(visual.TextStim(win, "BLANK"))
+    frames.append(visual.TextStim(win, " "))
     #cue
     start0=[gPar.r[0]*math.cos(d2r(cueAngle)),gPar.r[0]*math.sin(d2r(cueAngle))]
     end0=[gPar.r[1]*math.cos(d2r(cueAngle)),gPar.r[1]*math.sin(d2r(cueAngle))]
@@ -66,7 +81,7 @@ def runTrial(lPar):
     line0=visual.Line(win,start0,end0)
     line1=visual.Line(win,start1,end1)
     frames.append(visual.BufferImageStim(win,stim=(line0, line1)))
-    frames.append(visual.TextStim(win, "BLANK2")) #2-up/1-down
+    frames.append(visual.TextStim(win, " ")) #2-up/1-down
     #target
     pos=(gPar.radius*math.cos(d2r(lPar.angle)),gPar.radius*math.sin(d2r(lPar.angle)))
     frames.append(visual.TextStim(win, gPar.let[lPar.target],pos=pos))
@@ -74,28 +89,27 @@ def runTrial(lPar):
     frames.append(visual.TextStim(win, gPar.mask[0],pos=pos))
     frames.append(visual.TextStim(win, gPar.mask[1],pos=pos))
     stamps=el.runFrames(win,frames,frameDurations,trialClock)
-    event.waitKeys()
+    ans=getResp()
+    return(ans,trl)
 
 
-def runBlock(cond):
-    for i in range(2):
+def runBlock(blk):
+    blk+=1
+
+    cond = rng.integers(0,2,1)
+    lPar.cond=cond
+
+    for i in range(3):
+        trl=i
         theta = rng.integers(0,360,1)
         lPar.angle=theta
 
-        cond = rng.integers(0,2,1)
-        lPar.cond=cond
-
-        target = rng.integers(0,2,1)
-        target = int(target)
+        target = int(rng.integers(0,2,1))
         lPar.target=target
-        
-        
-        #lPar.dur=
-        
-        runTrial(lPar)
 
+        ans=runTrial(lPar,trl)
 
-
+        print(pid,sid,blk,trl,cond,gPar.let[lPar.target],lPar.dur,ans)
 
 message=visual.TextStim(win,"Press a key to start")
 message.draw()
@@ -104,10 +118,8 @@ event.waitKeys()
 
 
 
-runBlock(0)
+runBlock(blk)
 win.close()
-
-# print("Difference Between Time Stamps:\n",np.diff(stamps),"\n")
-# print("Difference Between Frames:\n",el.actualFrameDurations(frameDurations,stamps))
+fptr.close
 core.quit()
 
