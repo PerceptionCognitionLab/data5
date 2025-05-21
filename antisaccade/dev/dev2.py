a96c5690636069a52654c6028adc80f6071aae24
SPECL EXP ACCT <exp.pcl.mu@gmail.com>
2025-05-20 23:00:02 -0700

    automatic update

--- antisaccade/dev/dev2.py
+++ antisaccade/dev/dev2.py
@@ -63,11 +63,9 @@ def getResp():
     resp = int(resp==gPar.keyList[1])
     return([resp,round(rt,3)])
 
-
 def runTrial(lPar):
     frames=[]
     frameDurations=[60,1,lPar.dur,5,5,5]
-    fixX,fixL,fixR,cXLR,box,targ,mask1,mask2=createStim()
 
     lPar.target = int(rng.integers(0,2,1))
     lPar.posTarg = int(rng.integers(0,2,1))  #0=left, 1=right
@@ -75,7 +73,8 @@ def runTrial(lPar):
         posCue=lPar.posTarg
     else:
         posCue=1-lPar.posTarg
-   
+    
+    fixX,fixL,fixR,cXLR,box,targ,mask1,mask2=createStim()
     frames.append(cXLR)
     box[posCue].fillColor=[1,1,1]
     frames.append(visual.BufferImageStim(win,stim=box+[fixX]))
@@ -86,9 +85,7 @@ def runTrial(lPar):
     frames.append(visual.BufferImageStim(win,stim=(fixX,fixL,fixR,mask2)))
     stamps=el.runFrames(win,frames,frameDurations,trialClock)
     ans=getResp()
-    print(posCue,lPar.posTarg)
     return(ans)
-
 
 def runBlock(blk):
     blockStart(blk)
@@ -101,7 +98,6 @@ def runBlock(blk):
     numCor=0
 
     for trl in range(4):
-
         [resp,rt]=runTrial(lPar)
         print(pid,sid,blk,trl,lPar.isCongruent,lPar.target,lPar.dur,resp,rt,sep=", ", file=fptr)
 
@@ -116,35 +112,150 @@ def runBlock(blk):
             lPar.dur = lPar.dur+3
             numCor=0
 
+def p1(): #click through
+    messagep1=visual.TextStim(win,"Practice 1: \n\nClick your way through each step of the experiment.",height=30)
+    messagep1.draw()
+    win.flip()
+    event.waitKeys()
+
+    fixX,fixL,fixR,cXLR,box,targ,mask1,mask2=createStim()
+    for trl in range(2):
+        cXLR.draw()
+        win.flip()
+        event.waitKeys()
+        cXLR.draw()
+        box[trl].fillColor=[1,1,1]
+        box[trl].draw()
+        win.flip()
+        core.wait(0.006)
+        cXLR.draw()
+        win.flip()
+        event.waitKeys()
+        cXLR.draw()    
+        targ.draw()
+        win.flip()
+        event.waitKeys()
+        cXLR.draw()
+        mask1.draw()
+        win.flip()
+        core.wait(0.1)
+        cXLR.draw()
+        mask2.draw()
+        win.flip()
+        core.wait(0.1)
+        cXLR.draw()
+        win.flip()
+        event.waitKeys()
+
+def p2(): #slow
+    messagep2=visual.TextStim(win,"Practice 2: \n\nLet's run the experiment SLOWLY.",height=30)
+    messagep2.draw()
+    win.flip()
+    event.waitKeys()
+
+    for trial in range(8):
+        Pframes=[]
+        PframeDurations=[60,3,130,15,15,15]
+
+        lPar.target = int(rng.integers(0,2,1))
+        lPar.posTarg = int(rng.integers(0,2,1))  #0=left, 1=right
+        if trial<=4:
+            posCue=lPar.posTarg
+        else:
+            posCue=1-lPar.posTarg
+
+        if trial==1:
+            cong=visual.TextStim(win,"Same side",height=20)
+            cong.draw()
+            win.flip()
+            core.wait(2)
+        elif trial==5:
+            incong=visual.TextStim(win,"Opposite side",height=20)
+            incong.draw()
+            win.flip()
+            core.wait(2)
+    
+        fixX,fixL,fixR,cXLR,box,targ,mask1,mask2=createStim()
+        Pframes.append(cXLR)
+        box[posCue].fillColor=[1,1,1]
+        Pframes.append(visual.BufferImageStim(win,stim=box+[fixX]))
+        box[posCue].fillColor=[-1,-1,-1]
+        Pframes.append(cXLR)
+        Pframes.append(visual.BufferImageStim(win,stim=(fixX,fixL,fixR,targ)))
+        Pframes.append(visual.BufferImageStim(win,stim=(fixX,fixL,fixR,mask1)))
+        Pframes.append(visual.BufferImageStim(win,stim=(fixX,fixL,fixR,mask2)))
+        stamps=el.runFrames(win,Pframes,PframeDurations,trialClock)
+        ans=getResp()
+
+def p3(): #real thing
+    messagep3=visual.TextStim(win,"Practice 3: \n\nLet's run the experiment AT SPEED.",height=30)
+    messagep3.draw()
+    win.flip()
+    event.waitKeys()
+
+    for trial in range(12):
+        Pframes=[]
+        PframeDurations=[60,1,lPar.dur,5,5,5]
+
+        lPar.target = int(rng.integers(0,2,1))
+        lPar.posTarg = int(rng.integers(0,2,1))  #0=left, 1=right
+        if trial<=6:
+            posCue=lPar.posTarg
+        else:
+            posCue=1-lPar.posTarg
+    
+        if trial==1:
+            cong=visual.TextStim(win,"Same side",height=20)
+            cong.draw()
+            win.flip()
+            core.wait(2)
+        elif trial==7:
+            incong=visual.TextStim(win,"Opposite side",height=20)
+            incong.draw()
+            win.flip()
+            core.wait(2)
+
+        fixX,fixL,fixR,cXLR,box,targ,mask1,mask2=createStim()
+        Pframes.append(cXLR)
+        box[posCue].fillColor=[1,1,1]
+        Pframes.append(visual.BufferImageStim(win,stim=box+[fixX]))
+        box[posCue].fillColor=[-1,-1,-1]
+        Pframes.append(cXLR)
+        Pframes.append(visual.BufferImageStim(win,stim=(fixX,fixL,fixR,targ)))
+        Pframes.append(visual.BufferImageStim(win,stim=(fixX,fixL,fixR,mask1)))
+        Pframes.append(visual.BufferImageStim(win,stim=(fixX,fixL,fixR,mask2)))
+        stamps=el.runFrames(win,Pframes,PframeDurations,trialClock)
+        ans=getResp()
 
 def blockStart(blk):
     messageText=f"Block {blk+1} \n\nPress key to start"
+    messageText.height=30
     message=visual.TextStim(win,messageText)
     message.draw()
     win.flip()
     event.waitKeys()
 
-def intro():
-    messageIntro=visual.TextStim(win,"Welcome to the experiment. \n\nWe will start with some training blocks.\n\nPress any key to begin training")
-    messageIntro.draw()
-    win.flip()
-    event.waitKeys()
-
-
 def startExp():
-    message=visual.TextStim(win,"Now we will start the experiment blocks. \n\nPress a key to continue.")
+    message=visual.TextStim(win,"Now we will start the experiment blocks. \n\nPress a key to continue.",height=30)
     message.draw()
     win.flip()
     event.waitKeys()
 
+def intro():
+    messageIntro=visual.TextStim(win,"Welcome to the experiment! \n\nWe will start with some training blocks.\n\nPress any key to begin training.",height=30)
+    messageIntro.draw()
+    win.flip()
+    event.waitKeys()
 
-
-#intro()
+intro()
+p1()
+#p2()
+#p3()
 #startExp()
-blocks=[0,1,2,3,4,5]
-for i in range(int(len(blocks))): 
-    blk=blocks[i]
-    runBlock(blk)
+#blocks=[0,1,2,3,4,5]
+#for i in range(int(len(blocks))): 
+    #blk=blocks[i]
+    #runBlock(blk)
 win.close()
 fptr.close
 core.quit()
