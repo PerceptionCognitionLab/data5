25f9109a94af2320568749eeb960a309271ffce2
SPECL EXP ACCT <exp.pcl.mu@gmail.com>
2025-10-01 23:00:03 -0700

    automatic update

--- percMoment/dev/simultaneous/dev4.py
+++ percMoment/dev/simultaneous/dev4.py
@@ -24,6 +24,9 @@ fptr=open(fname,"w")
 win = visual.Window(units="pix", size=(500, 500), color=[-1, -1, -1], fullscr=True)
 mouse = event.Mouse(visible=False, newPos=[0,0], win=win) 
 trialClock=core.Clock()
+correctSound1=sound.Sound(value=500,secs=.1)
+correctSound2=sound.Sound(value=1000,secs=.2)
+errorSound=sound.Sound(value=300,secs=.5)
 seed = random.randrange(1e6)
 random.seed(seed)
 
@@ -46,7 +49,7 @@ def runTrial(dur, stimCode):
     if keys[0][0]=='x':
         resp=0
     return(resp)
-    # resp "1/m" means same and "0/x" is diff ?
+    # resp "1/m" means same and "0/x" is different
 
 
 
@@ -54,7 +57,7 @@ def runSim(trialNum):
     for i in range(trialNum):
         trialNum = i
         stim = random.choice([0,1,2])
-        dur = random.choice([2,4,6])
+        dur = random.choice([1,2,4,6])
         resp=runTrial(dur,stim)
         # staircase?
         info=[trialNum, dur, stim, resp]
@@ -64,26 +67,42 @@ def runSim(trialNum):
             info[2] = 0
         print(*info, sep=' ', file=fptr)
 
-
-runSim(15)
-
+# runSim(15)
 
 
+#############
+# staircase
+#############
+# 2 up 1 down
+def runSimult(trialNum):
+    counter = 0
+    dur = 6
+    for i in range(trialNum):
+        trialNum = i
+        stim = random.choice([0,1,2])
+        resp=runTrial(dur,stim)
+        info=[trialNum, dur, stim, resp]
+        if info[2]==2:
+            info[2] = 1
+        else:
+            info[2] = 0
+        print(*info, sep=' ', file=fptr)
+        # staircase
+        if (info[2]==info[3])&(counter==0):
+            counter+=1
+        elif (info[2]==info[3])&(counter==1):
+            dur = dur-2
+            if dur<0:
+                dur=0
+            counter=0
+        else:
+            dur = dur+2
+            if dur>8:
+                dur=8
+            counter=0
 
 
-
-
-
-# frames = [blank, both, blank]
-# frameTimes = [100,100,1]
-# elib.runFrames (win, frames, frameTimes, trialClock)
-
-# frames = [blank, stim2,both, blank]
-# frameTimes = [100,2,100,1]
-# elib.runFrames (win, frames, frameTimes, trialClock)
-
-# a=event.waitKeys()
-
+runSimult(15)
 
 fptr.close()
 win.close()
